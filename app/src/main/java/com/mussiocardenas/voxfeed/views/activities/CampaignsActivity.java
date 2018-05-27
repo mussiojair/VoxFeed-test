7f30c2b8315754fb5c0b03e061057fa39080baa3
mussiojair <mussiojair@gmail.com>
2018-05-26 21:46:22 -0500

    Cambios Visuales Navigator

--- app/src/main/java/com/mussiocardenas/voxfeed/views/activities/CampaignsActivity.java
+++ app/src/main/java/com/mussiocardenas/voxfeed/views/activities/CampaignsActivity.java
@@ -1,10 +1,12 @@
 package com.mussiocardenas.voxfeed.views.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.mussiocardenas.voxfeed.R;
 import com.mussiocardenas.voxfeed.presenters.CampaignElement;
@@ -20,6 +22,9 @@ public class CampaignsActivity extends AppCompatActivity
 
 
     private CampaignsPresenter presenter;
+    private TabLayout mTabs;
+    private android.support.v7.app.ActionBar mActionBar;
+
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -29,12 +34,34 @@ public class CampaignsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaigns_activity);
 
+        mActionBar = getSupportActionBar();
+
         presenter = new CampaignsPresenter(this);
+        mTabs = (TabLayout) findViewById(R.id.tabs);
+
+        initView();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.campaignRecycler);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.campaigns_menu, menu);
+        return true;
+    }
+
+    private void initView(){
+
+
+        mTabs.addTab(mTabs.newTab().setText(R.string.inicio));
+        mTabs.addTab(mTabs.newTab().setText(R.string.publicaciones));
+
+        mActionBar.setElevation(0);
 
     }
 
